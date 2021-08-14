16eaeee57879ab5f02a6c53d51ecf77caab3f819
vriznet <vriz@vriz.net>
2021-08-14 23:35:40 +0900

    8.3 ~ 8.4. Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,24 @@
-const App = () => <div></div>;
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import NotFound from './screens/NotFound';
+
+const App = () => {
+  const isLoggedIn = false;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
