4f9a33679550e063a33e7a252b0f5d35ece0ed12
vriznet <vriz@vriz.net>
2021-08-15 21:19:47 +0900

    8.8. Theme on Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { useReactiveVar } from '@apollo/client';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { isLoggedInVar } from './apollo';
+import { darkModeVar, isLoggedInVar } from './apollo';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import NotFound from './screens/NotFound';
+import { ThemeProvider } from 'styled-components';
 
 const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
+  const darkMode = useReactiveVar(darkModeVar);
+
+  const darkTheme = {
+    fontColor: 'white',
+    bgColor: 'black',
+  };
+
+  const lightTheme = {
+    fontColor: 'black',
+    bgColor: 'white',
+  };
+
   return (
-    <div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router>
         <Switch>
           <Route exact path="/">
@@ -19,7 +32,7 @@ const App = () => {
           </Route>
         </Switch>
       </Router>
-    </div>
+    </ThemeProvider>
   );
 };
 
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,4 +1,18 @@
-import { isLoggedInVar } from '../apollo';
+import styled from 'styled-components';
+import { darkModeVar, isLoggedInVar } from '../apollo';
 
-const Login = () => <h1 onClick={() => isLoggedInVar(true)}>Login</h1>;
+const Container = styled.div`
+  color: ${(props) => props.theme.fontColor};
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <h1 onClick={() => isLoggedInVar(true)}>Login</h1>
+      <button onClick={() => darkModeVar(false)}>Light</button>
+      <button onClick={() => darkModeVar(true)}>Dark</button>
+    </Container>
+  );
+};
 export default Login;
