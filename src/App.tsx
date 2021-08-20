33d6c612dbf2987e71b100d87f333dabb8f52854
vriznet <vriz@vriz.net>
2021-08-20 12:41:26 +0900

    10.9. Apollo Client

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { useReactiveVar } from '@apollo/client';
+import { ApolloProvider, useReactiveVar } from '@apollo/client';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
-import { darkModeVar, isLoggedInVar } from './apollo';
+import { client, darkModeVar, isLoggedInVar } from './apollo';
 import Home from './screens/Home';
 import Login from './screens/Login';
 import NotFound from './screens/NotFound';
@@ -15,26 +15,28 @@ const App = () => {
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Switch>
-            <Route exact path={routes.home}>
-              {isLoggedIn ? <Home /> : <Login />}
-            </Route>
-            {!isLoggedIn ? (
-              <Route path={routes.signUp}>
-                <SignUp />
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Switch>
+              <Route exact path={routes.home}>
+                {isLoggedIn ? <Home /> : <Login />}
               </Route>
-            ) : null}
-            <Route>
-              <NotFound />
-            </Route>
-          </Switch>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+              {!isLoggedIn ? (
+                <Route path={routes.signUp}>
+                  <SignUp />
+                </Route>
+              ) : null}
+              <Route>
+                <NotFound />
+              </Route>
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 };
 
--- src/apollo.ts
+++ src/apollo.ts
@@ -1,4 +1,14 @@
-import { makeVar } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  makeVar,
+  NormalizedCacheObject,
+} from '@apollo/client';
 
 export const isLoggedInVar = makeVar(false);
 export const darkModeVar = makeVar(false);
+
+export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: 'http://localhost:4000/graphql',
+});
