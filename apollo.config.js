f23b56ec00da6ac57392fe5a4426f283d2af1882
vriznet <vriz@vriz.net>
2021-10-08 12:04:28 +0900

    apollo config url -> process.env.NODE_ENV

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,10 @@ module.exports = {
   client: {
     service: {
       name: 'instaclone3-backend',
-      url: 'https://na-instaclone-3.herokuapp.com/graphql/',
+      url:
+        process.env.NODE_ENV === 'production'
+          ? 'https://na-instaclone-3.herokuapp.com/graphql/'
+          : 'http://localhost:4000/graphql',
     },
     includes: ['./src/**/*.{tsx,ts}'],
     tagName: 'gql',
